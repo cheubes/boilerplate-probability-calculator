9538a96c442d66ef096884517f4e0973be618dfa
cheubes <christophe@heubes.com>
2021-01-25 18:03:07 +0100

    Pass test_hat_class_contents.

--- prob_calculator.py
+++ prob_calculator.py
@@ -4,5 +4,11 @@ import random
 
 class Hat:
 
+    def __init__(self, **kwargs):
+        self.contents = []
+        for key, value in kwargs.items() :
+            for i in range(0, value) :
+                self.contents.append(key)
 
-def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
+
+# def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
