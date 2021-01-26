d8cad2447b0084c1d2acc51ab449feef4a739574
cheubes <christophe@heubes.com>
2021-01-26 09:56:48 +0100

    Pass test_hat_draw.

--- prob_calculator.py
+++ prob_calculator.py
@@ -10,5 +10,11 @@ class Hat:
             for i in range(0, value) :
                 self.contents.append(key)
 
+    def draw(self, nb):
+        result = []
+        for i in range(0, nb) :
+            result.append(self.contents.pop(random.randrange(len(self.contents))))
+        return result
+
 
 # def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
--- test_module.py
+++ test_module.py
@@ -9,15 +9,15 @@ class UnitTests(unittest.TestCase):
         expected = ["red","red","red","blue","blue"]
         self.assertEqual(actual, expected, 'Expected creation of hat object to add correct contents.')
 
-    # def test_hat_draw(self):
-    #     hat = prob_calculator.Hat(red=5,blue=2)
-    #     actual = hat.draw(2)
-    #     expected = ['blue', 'red']
-    #     self.assertEqual(actual, expected, 'Expected hat draw to return two random items from hat contents.')
-    #     actual = len(hat.contents)
-    #     expected = 5
-    #     self.assertEqual(actual, expected, 'Expected hat draw to reduce number of items in contents.')
-    #
+    def test_hat_draw(self):
+        hat = prob_calculator.Hat(red=5,blue=2)
+        actual = hat.draw(2)
+        expected = ['blue', 'red']
+        self.assertEqual(actual, expected, 'Expected hat draw to return two random items from hat contents.')
+        actual = len(hat.contents)
+        expected = 5
+        self.assertEqual(actual, expected, 'Expected hat draw to reduce number of items in contents.')
+
     # def test_prob_experiment(self):
     #     hat = prob_calculator.Hat(blue=3,red=2,green=6)
     #     probability = prob_calculator.experiment(hat=hat, expected_balls={"blue":2,"green":1}, num_balls_drawn=4, num_experiments=1000)
